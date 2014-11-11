c26476169e417e882b15db0e3045809d968c3a14
Harrison Zhao <roarofpie@gmail.com>
2014-11-11 11:13:24 -0500

    update

--- config/settings/secrets.js
+++ config/settings/secrets.js
@@ -1,28 +1,5 @@
-function getExternalIp() {
-  var ifconfig = require('os').networkInterfaces();
-  var device, i, I, protocol;
- 
-  for (device in ifconfig) {
-    // ignore network loopback interface
-    if (device.indexOf('lo') !== -1 || !ifconfig.hasOwnProperty(device)) {
-      continue;
-    }
-    for (i=0, I=ifconfig[device].length; i<I; i++) {
-      protocol = ifconfig[device][i];
- 
-      // filter for external IPv4 addresses
-      if (protocol.family === 'IPv4' && protocol.internal === false) {
-        console.log('inet address: ', protocol.address);
-        return protocol.address;
-      }
-    }
-  }
- 
-  return null;
-}
-
 //update accordingly
-module.exports = {
+var obj = {
   mysqlConfigs: {
     host: process.env.MODE==='production' ? 'localhost' : 'infcs.cloudapp.net',
     user: 'ics',
@@ -34,5 +11,33 @@ module.exports = {
   host: process.env.HOST || 'localhost',
   port: process.env.PORT || 3000,
   //inetAddr: 'infcs.cloudapp.net'
-  inetAddr: process.env.MODE==='production' ? getExternalIp() :'localhost:3000'
+  inetAddr: 'localhost:3000'
 };
+
+function getExternalIp() {
+  require('http').request({
+      hostname: 'fugal.net',
+      path: '/ip.cgi',
+      agent: false
+      }, function(res) {
+      if(res.statusCode !== 200) {
+          throw new Error('non-OK status: ' + res.statusCode);
+      }
+      res.setEncoding('utf-8');
+      var ipAddress = '';
+      res.on('data', function(chunk) { ipAddress += chunk; });
+      res.on('end', function() {
+          // ipAddress contains the external IP address
+          console.log('ip address: ' + ipAddress);
+          obj.inetAddr = ipAddress;
+      });
+      }).on('error', function(err) {
+      throw err;
+  }).end();
+}
+
+if (process.env.MODE === 'production') {
+  getExternalIp();
+}
+
+module.exports = obj;
